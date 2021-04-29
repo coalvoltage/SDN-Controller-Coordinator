b1eaa437959ecd3a70fe48fe3727489d5a0cb8a5
None <None>
2021-04-29 11:36:31 -0700

    Formatting

--- sdn_controller.py
+++ sdn_controller.py
@@ -7,7 +7,6 @@ class SDNControllerFat (object):
   def __init__ (self, connection):
     self.connection = connection
     connection.addListeners(self)
-
     self.mac_to_port = {}
 
 
@@ -25,20 +24,16 @@ class SDNControllerFat (object):
     """
     Handles packet in messages from the switch.
     """
-
     packet = event.parsed # This is the parsed packet data.
     if not packet.parsed:
       log.warning("Ignoring incomplete packet")
       return
-
     packet_in = event.ofp
-
     if str(packet.src) not in self.mac_to_port:
         self.mac_to_port[str(packet.src)] = packet_in.in_port
 
     if str(packet.dst) in self.mac_to_port:
       out_port = self.mac_to_port[str(packet.dst)]
-      
       #drop packet if destination port = src port)
       if self.mac_to_port[str(packet.dst)] == event.port:
         msg = of.ofp_flow_mod()
@@ -48,10 +43,8 @@ class SDNControllerFat (object):
         msg.buffer_id = packet_in.buffer_id
         self.connection.send(msg)
         return
-      
       #sourceMessage = "Source: " + str(packet.src) +  ", Dest: " + str(packet.dst) + ", Port:" + str(packet_in.in_port)
       #log.debug(sourceMessage)
-
       msg = of.ofp_flow_mod()
       msg.match = of.ofp_match.from_packet(packet)
       msg.data = packet_in
@@ -63,7 +56,6 @@ class SDNControllerFat (object):
       msg.match.dl_dst = packet.dst
       msg.actions.append(of.ofp_action_output(port=out_port))
       self.connection.send(msg)
-
     else:
       self.resend_packet(packet_in, of.OFPP_FLOOD)
       
